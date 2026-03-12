0cc84291ff35704342422ab648d679d96bb77d3e
Fabio Sierra <Pc@DESKTOP-93EEOL0>
2026-03-12 01:26:04 -0500

    Creacion de Apartado proyectos, cambios en el color de fondo, creación de Apartado skills

--- portafoliofabio/app/layout.jsx
+++ portafoliofabio/app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css"
 export default function page({ children }){
     return (
         <html>
-            <body className="bg-gradient-to-b from-ArribaYBajo via-medio to-ArribaYBajo max-w-7xl min-h-screen mx-auto">
+            <body className="bg-gradient-to-b from-Arriba via-black to-Abajo max-w-7xl min-h-screen mx-auto">
                 {children}
             </body>
         </html>
